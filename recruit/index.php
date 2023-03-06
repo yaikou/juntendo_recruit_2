9229cfcf08ded323d00d264eec12662b453a291d
Youhei Aikou <mobinc.yaikou@gmail.com>
2023-03-06 15:25:41 +0900

    first commit
    juntendo_recruitからひきつぎ（フォルダ階層構造変更）

---
+++ recruit/index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" id="viewport">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="google" content="nositelinkssearchbox">
+	<title></title>
+	<meta property="og:title" content="">
+	<meta property="og:description" content="">
+	<meta property="og:url" content="">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+
+	<!-- インデックス回避、後で削除↓ -->
+	<meta name="robots" content="noindex , nofollow" />
+
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/_include/header_files.html'; ?>
+	<link rel="stylesheet" href="css/header.css">
+	<link rel="stylesheet" href="css/footer.css">
+	<link rel="stylesheet" href="css/page_front.css">
+</head>
+
+<body class="front">
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/_include/header.html'; ?>
+	<main>
+		<div class="front_mainvisual" id="js-header-observer">
+			<img class="caption" src="/img/hero-text.svg" alt="">
+			<!-- slick↓-->
+			<ul id="main-slider" class="main-slider">
+				<li>
+					<picture>
+						<source srcset="/img/top__mv_01.webp" type="image/webp">
+						<img src="/img/top__mv_01.jpg" alt="">
+					</picture>
+				</li>
+				<li>
+					<picture>
+						<source srcset="/img/top__mv_02.webp" type="image/webp">
+						<img src="/img/top__mv_02.jpg" alt="">
+					</picture>
+				</li>
+				<li>
+					<picture>
+						<source srcset="/img/top__mv_03.webp" type="image/webp">
+						<img src="/img/top__mv_03.jpg" alt="">
+					</picture>
+				</li>
+				<li>
+					<picture>
+						<source srcset="/img/top__mv_04.webp" type="image/webp">
+						<img src="/img/top__mv_04.jpg" alt="">
+					</picture>
+				</li>
+			</ul>
+			<!-- slick↑-->
+		</div>
+
+		<section class="sec-news">
+			<div class="container d-flex">
+				<div class="g-headline-wrap">
+					<p class="g-headline-treat">News</p>
+					<h2 class="g-headline">採用ニュース</h2>
+					<a href="" class="link">一覧</a>
+				</div>
+				<ul class="news-list">
+					<li class="news-item">
+						<a class="link" href="">
+							<dl class="d-flex">
+								<dt>
+									<p class="date noto-sans">2023.02.02 (thu)</p>
+									<div class="cat-wrap d-flex">
+										<p class="cat-item job-info">採用情報</p>
+										<p class="cat-item others">その他</p>
+									</div>
+								</dt>
+								<dd class="noto-sans">研究補助員（パートタイム職員・医学部衛生学・公衆衛生学講座（公衆衛生学））募集について</dd>
+							</dl>
+						</a>
+					</li>
+					<li class="news-item">
+						<a class="link" href="">
+							<dl class="d-flex">
+								<dt>
+									<p class="date noto-sans">2023.02.02 (thu)</p>
+									<div class="cat-wrap d-flex">
+										<p class="cat-item job-info">採用情報</p>
+										<p class="cat-item others">その他</p>
+									</div>
+								</dt>
+								<dd class="noto-sans">事務補助員（パートタイム職員・オルガノイド開発研究講座　秘書業務）</dd>
+							</dl>
+						</a>
+					</li>
+					<li class="news-item">
+						<a class="link" href="">
+							<dl class="d-flex">
+								<dt>
+									<p class="date noto-sans">2023.01.27 (fri)</p>
+									<div class="cat-wrap d-flex">
+										<p class="cat-item job-info">採用情報</p>
+										<p class="cat-item medical-engineer">医療技術職</p>
+									</div>
+								</dt>
+								<dd class="noto-sans">介護福祉士（練馬病院）募集について</dd>
+							</dl>
+						</a>
+					</li>
+					<li class="news-item">
+						<a class="link" href="">
+							<dl class="d-flex">
+								<dt>
+									<p class="date noto-sans">2023.01.27 (fri)</p>
+									<div class="cat-wrap d-flex">
+										<p class="cat-item job-info">採用情報</p>
+										<p class="cat-item office-worker">事務職員</p>
+									</div>
+								</dt>
+								<dd class="noto-sans">事務員　臨床研究・治験センター</dd>
+							</dl>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</section>
+		<section class="sec-about">
+			<div class="bottom-layer container">
+				<div class="bg"></div>
+				<div class="flex-wrapper d-flex">
+					<div class="g-headline-wrap">
+						<p class="g-headline-treat">About Juntendo</p>
+						<h2 class="g-headline">順天堂について</h2>
+					</div>
+					<a class="external-link" href="" target="_blank" rel="noopener noreferrer">
+						<picture>
+							<source srcset="/img/top__about-banner.webp" type="image/webp">
+							<img class="banner" src="/img/top__about-banner.jpg" alt="180 YEARS HISTORY JUNTENDO 人在りて、我在り。">
+						</picture>
+						<img class="link-icon" src="/img/icon_external-link.svg" alt="">
+					</a>
+				</div>
+				<div class="desc-wrap d-flex">
+					<div class="bg"></div>
+					<picture>
+						<source srcset="/img/top__about-img.webp" type="image/webp">
+						<img class="about-img" src="/img/top__about-img.jpg" alt="">
+					</picture>
+					<div class="desc">
+						<h3 class="noto-serif">本学で働くということ</h3>
+						<p>この文章はダミーで、文字の量やサイズを把握する為のものです。この文章はダミーで、文字の量やサイズを把握する為のものです。この文章はダミーで、文字の量やサイズを把握する為のものです。この文章はダミーで、文字の量やサイズを把握する為のものです。この文章はダミーで、文字の量やサイズを把握する為のものです。</p>
+						<a href="" class="more barlow">Read more<i class="fa-sharp fa-solid fa-angle-right"></i></a>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="sec-recruit">
+			<div class="bottom-layer container">
+				<div class="bg"></div>
+				<p class="g-headline-treat">Recruit</p>
+				<h2 class="g-headline">採用情報</h2>
+				<p class="desc">この文章はダミーで、文字の量やサイズを把握する為のものです。この文章はダミーで、文字の量やサイズを把握する為のものです。この文章はダミーで、文字の量やサイズを把握する為のものです。この文章はダミーで、文字の量やサイズを把握する為のものです。</p>
+				<section class="sec-job-info">
+					<h3 class="noto-serif">学校法人 順天堂</h3>
+					<ul class="d-flex">
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-img01.webp" type="image/webp">
+									<img src="/img/top__recruit-img01.jpg" alt="">
+								</picture>
+								<p class="noto-sans">教員</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-img02.webp" type="image/webp">
+									<img src="/img/top__recruit-img02.jpg" alt="">
+								</picture>
+								<p class="noto-sans">看護師</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-img03.webp" type="image/webp">
+									<img src="/img/top__recruit-img03.jpg" alt="">
+								</picture>
+								<p class="noto-sans">医療技術職員</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-img04.webp" type="image/webp">
+									<img src="/img/top__recruit-img04.jpg" alt="">
+								</picture>
+								<p class="noto-sans">事務総合職</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-img05.webp" type="image/webp">
+									<img src="/img/top__recruit-img05.jpg" alt="">
+								</picture>
+								<p class="noto-sans">その他採用情報<br>（事務員・技術員等）</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-img06.webp" type="image/webp">
+									<img src="/img/top__recruit-img06.jpg" alt="">
+								</picture>
+								<p class="noto-sans">障がいをお持ちの方</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+					</ul>
+				</section>
+				<section class="sec-hospital-job-info">
+					<h3 class="noto-serif">附属病院採用情報</h3>
+					<ul class="d-flex">
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-hospital_juntendo.webp" type="image/webp">
+									<img src="/img/top__recruit-hospital_juntendo.jpg" alt="">
+								</picture>
+								<p class="noto-sans">順天堂医院</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-hospital_shizuoka.webp" type="image/webp">
+									<img src="/img/top__recruit-hospital_shizuoka.jpg" alt="">
+								</picture>
+								<p class="noto-sans">静岡医院</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-hospital_urayasu.webp" type="image/webp">
+									<img src="/img/top__recruit-hospital_urayasu.jpg" alt="">
+								</picture>
+								<p class="noto-sans">浦安病院</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-hospital_koshigaya.webp" type="image/webp">
+									<img src="/img/top__recruit-hospital_koshigaya.jpg" alt="">
+								</picture>
+								<p class="noto-sans">順天堂越谷病院</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-hospital_juntendo.webp" type="image/webp">
+									<img src="/img/top__recruit-hospital_juntendo.jpg" alt="">
+								</picture>
+								<p class="noto-sans">順天堂江東高齢者<br>医療センター</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<picture>
+									<source srcset="/img/top__recruit-hospital_nerima.webp" type="image/webp">
+									<img src="/img/top__recruit-hospital_nerima.jpg" alt="">
+								</picture>
+								<p class="noto-sans">練馬病院</p>
+								<img class="r-arrow" src="/img/r-arrow.svg" alt="">
+							</a>
+						</li>
+					</ul>
+				</section>
+			</div>
+		</section>
+	</main>
+
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/_include/footer.html'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/_include/footer_files.html'; ?>
+	<script src="/js/home.js"></script>
+</body>
+
+</html>
